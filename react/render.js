09296b9238a54a7fdc06153d6d6f03194368a139
lazylwz <liwenzhong.lwz.cn@gmail.com>
2022-10-18 17:19:11 +0800

    feat：Concurrent Mode

--- react/render.js
+++ react/render.js
@@ -6,8 +6,28 @@ const render = (element, container) => {
   Object.keys(element.props)
     .filter((key) => key !== "children")
     .forEach((name) => (dom[name] = element.props[name]));
-  element.props.children.forEach((child) => render(child, dom));
   container.appendChild(dom);
 };
 
+let nextUnitOfWork = null;
+
+// 调度函数
+const workLoop = (deadline) => {
+  // 不应该交出控制权或不应该停止
+  let shouldYield = false;
+  while (nextUnitOfWork && !shouldYield) {
+    // 执行工作单元
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    // 检测浏览器是否还有空余时间
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+  // 没有空余时间，将工作放到浏览器下一次空闲时执行
+  requestIdleCallback(workLoop);
+};
+// 第一次请求
+requestIdleCallback(workLoop);
+
+const performUnitOfWork = (work) => {
+  // TODO
+};
 export default render;
